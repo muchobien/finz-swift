9baef16b9e8ed661228e02b1613b8e9d8f52ace0
Rodrigo Sanabria <krosf@muchobien.dev>
2022-06-05 19:33:29 +0200

    Initial Commit

---
+++ Finz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finz
+//
+//  Created by Rodrigo Sanabria on 5/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finz/FinzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinzApp.swift
+//  Finz
+//
+//  Created by Rodrigo Sanabria on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FinzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
